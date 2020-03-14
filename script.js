ffc42a92e654e1f7ba251cf370ad73459fd2bcca
ALee4428 <alee4428@yahoo.com>
2020-03-13 21:46:06 -0400

    code clean-up; activated external links

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ var quotes = [
   '"I am glad I will not be young in a future without wilderness." -Aldo Leopold',
   '"Live in each season as it passes; breathe the air, drink the drink, taste the fruit, and resign yourself to the influence of the earth." -Henry David Thoreau, Walden',
   '"The poetry of the earth is never dead." -John Keats',
-  '"The creation of a thousand forests is in one acorn" -Ralph Waldo Emerson',
+  '"The creation of a thousand forests is in one acorn" -Ralph Waldo Emerson'
 ]
 
 function newQuote(){
